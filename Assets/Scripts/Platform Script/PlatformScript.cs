03b4c81d644a2b41d5de88a953bcf1675b6d16fa
Fariz Baiquni <farizbaiquni@gmail.com>
2020-03-31 10:12:55 +0700

    Menambahkan backgorund, standard platform, breakable platform, spike platform, moving right platform, moving left platform, prefabs, animasi breakable platform

---
+++ Assets/Scripts/Platform Script/PlatformScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour{
+
+    public float moveSpeed = 2f;
+    public float boundY = 6f;
+
+    public bool movingPlatformLeft, movingPlatformRight, isBreakable, isSpike, isPlatform;
+
+    private Animator anim;
+
+    void Awake(){
+        
+        if(isBreakable){
+            anim = GetComponent<Animator>();
+        }
+
+    }
+
+    void Update(){
+        Move();
+    }
+
+    void Move(){
+
+        Vector2 temp = transform.position;
+        temp.y += moveSpeed * Time.deltaTime;
+        transform.position = temp;
+
+        if(temp.y >= boundY){
+            gameObject.SetActive(false);
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
